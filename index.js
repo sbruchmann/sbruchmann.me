c30d4947d4f76fe5d328e89af2fb3768abb9f99a
Steffen Bruchmann <hi@sbruchmann.me>
2015-10-26 00:46:11 +0100

    Render markdown files as HTML

--- index.js
+++ index.js
@@ -4,11 +4,27 @@
 const Aldous = require('aldous')
 const fs = require('./fs')
 const config = require('./config')
+const markdown = require('markdown-it')
 
 // Setup
 let build = Aldous(config)
 let dest = config.paths.destination
 let src = config.paths.source
+
+build
+  // Render markdown files as HTML
+  .use(function(files, aldous, done) {
+    let md = markdown()
+    let mdExt = /\.md$/
+
+    setImmediate(done)
+    files.forEach(function(file) {
+      if (mdExt.test(file.path)) {
+        file.path = file.path.replace(mdExt, '.html')
+        file.source = new Buffer(md.render(file.source.toString()))
+      }
+    })
+  })
 
 // Do ya thing!
 fs.readSourceDir(src)
