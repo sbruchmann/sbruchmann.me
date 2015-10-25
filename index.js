a994f2dd49b368ae3e0ecfb3823da9f75ddc81b5
Steffen Bruchmann <hi@sbruchmann.me>
2015-10-25 23:27:56 +0100

    Recursively read source directory

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict';
 
 const Aldous = require('aldous')
+const fs = require('fs')
+const path = require('path')
+const readdir = require('recursive-readdir')
 
-Aldous()
-  .run([], function callback(err, output) {
-    if (err) throw err
-    console.log(output)
+function readSourceDir(dir) {
+  return new Promise(function resolver(resolve, reject) {
+    readdir(dir, function callback(err, contents) {
+      if (err) return reject(err)
+      resolve(contents.map(function(file) {
+        return {
+          path: path.relative(dir, file)
+        }
+      }))
+    })
   })
+}
+
+readSourceDir(__dirname + '/src')
+  .then(function(input) {
+    return Aldous().run(input)
+  })
+  .then(function callback(response) {
+    console.log('output', response[0])
+  })
+  .catch(console.error)
