472b90dc7e3db885d48421881feaebc6deb7e3e6
Steffen Bruchmann <hi@sbruchmann.me>
2015-10-26 16:26:37 +0100

    Fix missing call to done()

--- processors/moment.js
+++ processors/moment.js
@@ -5,6 +5,7 @@ const moment = require('moment')
 
 module.exports = function setup() {
   return function plugin(files, aldous, done) {
+    setImmediate(done)
     files.forEach(function(file) {
       if (file.date) {
         file.date = moment(file.date)
