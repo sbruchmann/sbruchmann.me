07b65a33e0774a5e45a81481a9ccdadb508b2f1a
Steffen Bruchmann <hi@sbruchmann.me>
2015-10-26 16:32:36 +0100

    Minify CSS in production

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Dependencies
+const cssnano = require('cssnano')
 const cssnext = require('cssnext')
 const moment = require('moment')
 const path = require('path')
@@ -8,6 +9,12 @@ const path = require('path')
 // Aliases
 let basedir = __dirname
 let env = process.env.NODE_ENV || 'development'
+let postcssProcessors = [cssnext()]
+
+// Minify CSS in production
+if (env === 'production') {
+  postcssProcessors.push(cssnano())
+}
 
 module.exports = {
   env: env,
@@ -38,9 +45,7 @@ module.exports = {
         return ['blog', file.category, file.slug + '.html'].join('/')
       }
     },
-    postcss: [
-      cssnext()
-    ]
+    postcss: postcssProcessors
   },
 
   globals: {
